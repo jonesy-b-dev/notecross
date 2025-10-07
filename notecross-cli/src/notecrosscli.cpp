bd3e0238096d33850a09911d69dbd900bb66dfb6
Jonas de Bruin <jonasdebruin@proton.me>
2025-10-07 11:06:17 +0200

    Function definitions

--- notecross-cli/src/notecrosscli.cpp
+++ notecross-cli/src/notecrosscli.cpp
@@ -4,6 +4,9 @@
 namespace NoteCrossCLI
 {
 
-void AddTask() { std::cout << "hello from notecrosscli!\n"; }
+void AddTask(std::string newTask) { std::cout << "hello from notecrosscli!\n"; }
+void UpdateTask(int id, std::string updatedTask) { std::cout << "Update task..\n"; }
+void RemoveTask(int id) { std::cout << "Remove task..\n"; }
+void ListTask() { std::cout << "Listing tasks..\n"; }
 
 } // namespace NoteCrossCLI

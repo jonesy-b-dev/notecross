0009087b6bb237a044fe90dd3a6e5abfd6695b6e
Jonas de Bruin <jonasdebruin@proton.me>
2025-10-08 13:13:48 +0200

    Added help option

--- notecross-cli/src/main.cpp
+++ notecross-cli/src/main.cpp
@@ -72,8 +72,32 @@ int main(int argc, char* argv[])
     {
         // NoteCrossCLI::AddTask();
     }
+    else if (option == "--help" || option == "-h")
+    {
+        std::cout << "--Notecross help--\n\n"
+                     "--add / -a {newTaskName} {newTaskDue}\n"
+                     "\tAdd a new task using the AddTask function\n\n"
+
+                     "--update / -u {taskId} {newTaskName} {newTaskDue}\n"
+                     "\tUpdate task using the TaskUpdate function\n\n"
+
+                     "--remove / -r {taskId}\n"
+                     "\tRemove task using the TaskRemove function\n\n"
+
+                     "--done / -d {taskId}\n"
+                     "\tMark task as done using the TaskDone function\n\n"
+
+                     "--list / -l\n"
+                     "\tList all the tasks that are open using the TaskGetAll() function\n\n"
+
+                     "--sync / -s\n"
+                     "\tManually sync the task file using the TaskSync option\n\n"
+
+                     "--help / -h\n"
+                     "\tList all CLI options\n";
+    }
     else
     {
-        std::cout << "No option given, `notecross help` to list options\n";
+        std::cout << "No valid option given, `notecross help` to list options\n";
     }
 }
--- notecross-cli/src/notecrosscli.cpp
+++ notecross-cli/src/notecrosscli.cpp
@@ -4,7 +4,7 @@
 namespace NoteCrossCLI
 {
 
-void AddTask(std::string newTask) { std::cout << "Added task"; }
+void AddTask(std::string newTask) { std::cout << "Added task\n"; }
 void UpdateTask(int id, std::string updatedTask) { std::cout << "Update task..\n"; }
 void RemoveTask(int id) { std::cout << "Remove task..\n"; }
 void ListTask() { std::cout << "Listing tasks..\n"; }

7dd22685ea011be36ba2f89175020ddbbbf9da36
Jonas de Bruin <jonasdebruin@proton.me>
2025-10-09 14:43:44 +0200

    Daemonizer abstraction

--- notecross-daemon/src/daemonizer.cpp
+++ notecross-daemon/src/daemonizer.cpp
@@ -1,38 +1,35 @@
+#pragma once
 #include <iostream>
 #include <sched.h>
 #include <unistd.h>
 
-int main()
+pid_t Daemonize()
 {
-    std::cout << "Started daemon!\n";
-
     // First Child process
     pid_t childPid = fork();
 
     if (childPid == -1)
     {
         std::cerr << "First fork failed!" << std::endl;
-        return 1;
+        return -1;
     }
     if (childPid > 0)
-        return 1;
+        return -1;
 
     std::cout << "First child process, PID: " << getpid() << std::endl;
 
     setsid();
 
     // Acutual daemon process
-    pid_t notecrossDaemon = fork();
-    if (notecrossDaemon == -1)
+    pid_t secondChild = fork();
+    if (secondChild == -1)
     {
-        std::cerr << "First fork failed!" << std::endl;
-        return 1;
+        std::cerr << "Second fork failed!" << std::endl;
+        return -1;
     }
-    if (notecrossDaemon > 0)
-        return 1;
+    if (secondChild > 0)
+        return -1;
 
     std::cout << "Notecross Daemon process, PID: " << getpid() << std::endl;
-    while (1)
-    {
-    }
+    return secondChild;
 }
---
+++ notecross-daemon/src/daemonizer.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include <sched.h>
+
+pid_t Daemonize();

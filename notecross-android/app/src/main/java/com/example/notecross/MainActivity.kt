0aeda21a05815f2e123ed2627cfc8259dff20b7c
Jonas de Bruin <jonasdebruin@proton.me>
2025-11-26 10:07:13 +0100

    Add button design

--- notecross-android/app/src/main/java/com/example/notecross/MainActivity.kt
+++ notecross-android/app/src/main/java/com/example/notecross/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -26,13 +24,9 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 )
                 {
-                    Row(
-                        horizontalArrangement = Arrangement.Center
-                    ){
                     Column() {
                         Title("Notecross")
                         AddTaskButton(onClick = { })
-                    }
                     }
                 }
             }
--- notecross-android/app/src/main/java/com/example/notecross/components/AddButton.kt
+++ notecross-android/app/src/main/java/com/example/notecross/components/AddButton.kt
@@ -1,5 +1,9 @@
 package com.example.notecross.components;
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
@@ -8,27 +12,35 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun AddTaskButton(
-    onClick: () -> Unit,
-    )
+fun AddTaskButton( onClick: () -> Unit)
 {
-    FloatingActionButton(
-        onClick = onClick,
-        shape = CircleShape,
-        containerColor = MaterialTheme.colorScheme.secondary,
+    Row(
         modifier = Modifier
-            .size(60.dp)
-    ) {
-        Icon(
-            imageVector = Icons.Default.Add,
-            tint = MaterialTheme.colorScheme.tertiary,
-            contentDescription = "Add Task",
+            .fillMaxSize()
+            .padding(bottom = 60.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.Bottom
+    )
+    {
+        FloatingActionButton(
+            onClick = onClick,
+            shape = CircleShape,
+            containerColor = MaterialTheme.colorScheme.secondary,
             modifier = Modifier
-                .size(50.dp)
-        )
+                .size(60.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                tint = MaterialTheme.colorScheme.tertiary,
+                contentDescription = "Add Task",
+                modifier = Modifier
+                    .size(50.dp)
+            )
+        }
     }
 }

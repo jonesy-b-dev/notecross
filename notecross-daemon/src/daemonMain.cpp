d08c5ca711fa976238f76e9f1f11fb1529c9f2ae
Jonas de Bruin <jonasdebruin@proton.me>
2026-09-06 23:23:43 +0200

    Display notification on task due

--- notecross-daemon/src/daemonMain.cpp
+++ notecross-daemon/src/daemonMain.cpp
@@ -5,8 +5,8 @@
 #include <log.hpp>
 #include <string>
 // #include <glib-2.0/glib.h">
-#include <unistd.h>
 #include <libnotify/notify.h>
+#include <unistd.h>
 
 int main()
 {
@@ -19,23 +19,54 @@ int main()
         NCShared::LogFileError("Failed to daemonize process with pid: " + std::to_string(getpid()));
         exit(1);
     }
+
     std::chrono::time_point now = std::chrono::system_clock::now();
     std::chrono::duration duration = now.time_since_epoch();
     long currentUnixTime = std::chrono::duration_cast<std::chrono::seconds>(duration).count();
 
-	NCShared::Task taskToNotify = CalculateNextNotification(currentUnixTime);
+    NCShared::Task taskToNotify = CalculateNextNotification(currentUnixTime);
 
-    if (taskToNotify.dueDate != 9999999999)
+    while (1)
     {
-		NCShared::LogFileMessage("Next task to notify: Description: " + taskToNotify.description);
-        notify_init("Task is due!");
-        NotifyNotification* n = notify_notification_new(taskToNotify.description.c_str(), " ", 0);
-        notify_notification_set_timeout(n, 5000); // 5 seconds
-
-        if (!notify_notification_show(n, 0))
+        if (taskToNotify.dueDate == 9999999999)
         {
-            NCShared::LogFileError("Failed to show notification");
+            // Wait for task file update
+        }
+        else
+        {
+            NCShared::LogFileMessage("Next task to notify: Description: " +
+                                     taskToNotify.description);
+            NCShared::LogFileMessage("Next notification if not interupted in: " +
+                                     std::to_string(taskToNotify.dueDate - currentUnixTime));
+
+            sleep(taskToNotify.dueDate - currentUnixTime + 1);
+            notify_init("Task is due!");
+            NotifyNotification* n =
+                notify_notification_new(taskToNotify.description.c_str(), " ", 0);
+            notify_notification_set_timeout(n, 5000); // 5 seconds
+
+            if (!notify_notification_show(n, 0))
+            {
+                NCShared::LogFileError("Failed to show notification");
+            }
+            std::chrono::time_point now = std::chrono::system_clock::now();
+            std::chrono::duration duration = now.time_since_epoch();
+            long currentUnixTime =
+                std::chrono::duration_cast<std::chrono::seconds>(duration).count();
+
+            NCShared::Task taskToNotify = CalculateNextNotification(currentUnixTime);
         }
     }
 
+    // Calculate earliest noti that needs to be shown
+    // Calculate when:
+    // Task file gets updated
+    // Deamon starts
+    // Notification has been send
+    // Wait till noti needs to be send
+    // Send noti
+    // Repeat
+
+    // CalcFunc
+    // Check all due dates
 }
